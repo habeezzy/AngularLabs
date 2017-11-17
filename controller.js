a232551e5433ece8b229c369ea456004cb2eced5
habeezzy <mhabeebulah@gmail.com>
2017-11-17 07:03:48 -0500

    "first commit"

--- controller.js
+++ controller.js
@@ -1,15 +1,13 @@
 (function() {
 function FormController() {
   var vm = this;
-  vm.newTask = [ "Grocery Shop", "Laundry", "Get Gas", "Vacuum"];
-    vm.removeTask = function() {
-     console.log(task);
-     vm.List.splice(task, 1);
+  vm.newTask = [ "Grocery Shop", "Laundry", "Get Gas"];
+    vm.removeTask = function(task) {
+     vm.newTask.splice(task, 1);
 
    };
-   vm.addTask = function(task) {
-     console.log(task);
-     vm.newTask.push(task, 1);
+   vm.addTask = function(add) {
+     vm.newTask.push(add);
    };
 }
    angular
